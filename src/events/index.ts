5e8fb6d06cee65498a4521100ad8aebf4169a7d7
neumanf <fabricionewman@protonmail.com>
2021-09-29 18:06:14 +0000

    refactor: custom commands handling

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,4 @@
-import { Bot, Composer } from "grammy";
+import { Bot, Composer, Context } from "grammy";
 import { getCustomCommandsFromDB, updateCustomCommand } from "../core/db/customCommand";
 
 import { ExtendedContext } from "../core/bot/context";
@@ -6,39 +6,38 @@ import { STOP_ICON } from "../utils/consts";
 import { ICommand } from "src/interfaces/command";
 import newChatMembers from "./newChatMembers";
 
-const events = (bot: Bot<ExtendedContext>): void => {
-    bot.on("message:new_chat_members", newChatMembers);
+async function handleCustomCommands(ctx: Context) {
+    const customCommands = await getCustomCommandsFromDB(Number(ctx?.message?.chat?.id));
+    const commandsList = new RegExp(`^\/(${customCommands.map((command: ICommand) => command.name).join("|")})$`);
+    const composer = new Composer();
 
-    // TODO: refactor
-    bot.on(
-        "message:text",
-        bot.lazy(async (ctx: ExtendedContext): Promise<Composer<any>> => {
-            const customCommands = await getCustomCommandsFromDB(Number(ctx?.message?.chat?.id));
+    composer.hears(commandsList, async (ctx: Context) => {
+        if (ctx.message?.text?.[0] === "/") {
+            for (let i = 0; i < customCommands.length; i++) {
+                try {
+                    if (customCommands[i].name === ctx.message?.text?.slice(1)) {
+                        if (customCommands[i].counter >= 0) {
+                            await ctx.reply(customCommands[i].value.replace("$COUNTER", `${customCommands[i].counter + 1}`));
 
-            const commandsList = new RegExp(`^\/(${customCommands.map((command: ICommand) => command.name).join("|")})$`);
-
-            return bot.hears(commandsList, async (ctx: ExtendedContext) => {
-                if (ctx.message?.text?.[0] === "/") {
-                    for (let i = 0; i < customCommands.length; i++) {
-                        try {
-                            if (customCommands[i].name === ctx.message?.text?.slice(1)) {
-                                if (customCommands[i].counter >= 0) {
-                                    await ctx.replyToMessage(customCommands[i].value.replace("$COUNTER", `${customCommands[i].counter + 1}`));
-
-                                    await updateCustomCommand(customCommands[i].name, customCommands[i].counter);
-                                } else {
-                                    await ctx.replyToMessage(customCommands[i].value);
-                                }
-                            }
-                        } catch (e) {
-                            console.error(e);
-                            await ctx.replyToMessage(`${STOP_ICON} Error: Could not send command.`);
+                            await updateCustomCommand(customCommands[i].name, customCommands[i].counter);
+                        } else {
+                            await ctx.reply(customCommands[i].value);
                         }
                     }
+                } catch (e) {
+                    console.error(e);
+                    await ctx.reply(`${STOP_ICON} Error: Could not send command.`);
                 }
-            });
-        }),
-    );
+            }
+        }
+    });
+
+    return composer;
+}
+
+const events = (bot: Bot<ExtendedContext>): void => {
+    bot.on("message:new_chat_members", newChatMembers);
+    bot.lazy(handleCustomCommands);
 };
 
 export default events;
