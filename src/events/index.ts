e503a36f19cf81e5c979061e19a5be766df72bba
neumanf <fabricionewman@protonmail.com>
2021-09-29 19:30:39 +0000

    refactor(events): move custom commands handler to new file

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,39 +1,8 @@
-import { Bot, Composer, Context } from "grammy";
-import { getCustomCommandsFromDB, updateCustomCommand } from "../core/db/customCommand";
+import { Bot } from "grammy";
 
 import { ExtendedContext } from "../core/bot/context";
-import { STOP_ICON } from "../utils/consts";
-import { ICommand } from "src/interfaces/command";
 import newChatMembers from "./newChatMembers";
-
-async function handleCustomCommands(ctx: Context) {
-    const customCommands = await getCustomCommandsFromDB(Number(ctx?.message?.chat?.id));
-    const commandsList = new RegExp(`^\/(${customCommands.map((command: ICommand) => command.name).join("|")})$`);
-    const composer = new Composer();
-
-    composer.hears(commandsList, async (ctx: Context) => {
-        if (ctx.message?.text?.[0] === "/") {
-            for (let i = 0; i < customCommands.length; i++) {
-                try {
-                    if (customCommands[i].name === ctx.message?.text?.slice(1)) {
-                        if (customCommands[i].counter >= 0) {
-                            await ctx.reply(customCommands[i].value.replace("$COUNTER", `${customCommands[i].counter + 1}`));
-
-                            await updateCustomCommand(customCommands[i].name, customCommands[i].counter);
-                        } else {
-                            await ctx.reply(customCommands[i].value);
-                        }
-                    }
-                } catch (e) {
-                    console.error(e);
-                    await ctx.reply(`${STOP_ICON} Error: Could not send command.`);
-                }
-            }
-        }
-    });
-
-    return composer;
-}
+import handleCustomCommands from "./customCommands";
 
 const events = (bot: Bot<ExtendedContext>): void => {
     bot.on("message:new_chat_members", newChatMembers);
