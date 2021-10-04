4f15f010d344db5ec082d90a0ee909da5fecac6e
neumanf <fabricionewman@protonmail.com>
2021-10-04 13:51:07 +0000

    feat: add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+import { GrammyError, HttpError } from "grammy";
+
 import bot from "./core/bot";
 import { development, production } from "./utils/launch";
 
@@ -10,6 +12,19 @@ import events from "./events";
 bot.use(commands);
 bot.use(actions);
 bot.use(events);
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
+});
 
 process.env.NODE_ENV === "development" ? development(bot) : production(bot);
 
