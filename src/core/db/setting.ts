695beaf707f027ccfe365ec6c78b4728f98de11f
neumanf <fabricionewman@protonmail.com>
2021-10-12 12:20:56 +0000

    refactor(db): better error handling

--- src/core/db/setting.ts
+++ src/core/db/setting.ts
@@ -2,19 +2,29 @@ import { ISetting } from "src/interfaces/setting";
 import Setting from "../../models/setting";
 
 const setSetting = async (_groupId: number, setting: string, value: unknown): Promise<ISetting | null> => {
-    return await Setting.findOneAndUpdate(
-        { groupId: _groupId },
-        {
-            $set: {
-                [setting]: value,
+    try {
+        return await Setting.findOneAndUpdate(
+            { groupId: _groupId },
+            {
+                $set: {
+                    [setting]: value,
+                },
             },
-        },
-        { upsert: true },
-    );
+            { upsert: true },
+        );
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 const getSettings = async (_groupId: number): Promise<ISetting | null> => {
-    return await Setting.findOne({ groupId: _groupId });
+    try {
+        return await Setting.findOne({ groupId: _groupId });
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 export { setSetting, getSettings };
--- src/core/db/warn.ts
+++ src/core/db/warn.ts
@@ -2,13 +2,23 @@ import User from "../../models/user";
 import { IUser } from "../../interfaces/user";
 
 export const getWarns = async (_id: number): Promise<number> => {
-    const user = await User.findOne({
-        id: _id,
-    });
+    try {
+        const user = await User.findOne({
+            id: _id,
+        });
 
-    return user?.warns ? user.warns : 0;
+        return user?.warns ? user.warns : 0;
+    } catch (error) {
+        console.error(error);
+        return 0;
+    }
 };
 
 export const updateWarns = async (_id: number, value: number): Promise<IUser | null> => {
-    return await User.findOneAndUpdate({ id: _id }, { $set: { warns: value } }, { upsert: true });
+    try {
+        return await User.findOneAndUpdate({ id: _id }, { $set: { warns: value } }, { upsert: true });
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
