33d0155f5e05e9531ddfdd72b2a04d2af2195f5d
neumanf <fabricionewman@protonmail.com>
2021-10-15 19:23:53 +0000

    fix: error handling

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
 export class Database {
-    static async connect(): Promise<void> {
+    static async connect(): Promise<typeof mongoose> {
         const { NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = process.env;
         const url =
             NODE_ENV === "production"
                 ? `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
                 : `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-        await mongoose
-            .connect(url)
-            .then(() => console.log("[DB] Connected successfully."))
-            .catch((e) => console.error("[DB] Error:", e));
+        return await mongoose.connect(url);
     }
 }
--- src/utils/launch.ts
+++ src/utils/launch.ts
@@ -1,4 +1,4 @@
-import { Bot, webhookCallback } from "grammy";
+import { Bot, GrammyError, HttpError, webhookCallback } from "grammy";
 import express from "express";
 
 import { ExtendedContext } from "../core/bot/context";
@@ -6,27 +6,52 @@ import { Database } from "../core/db";
 
 export class Launch {
     static async production(bot: Bot<ExtendedContext>): Promise<express.Application> {
-        await Database.connect();
-
         const { WEBHOOK_URL, SECRET_PATH, PORT } = process.env;
         const app = express();
 
         app.use(express.json());
         app.use(`/${SECRET_PATH}`, webhookCallback(bot, "express"));
 
-        app.listen(PORT, async () => {
-            await bot.api.setWebhook(`${WEBHOOK_URL}/${SECRET_PATH}`);
-            console.log(`[SERVER] Bot starting webhook`);
+        app.use((err, req, res, next) => {
+            console.error(err);
+            next();
         });
+
+        await Database.connect()
+            .then(() => {
+                console.log("[DB] Connected successfully.");
+
+                app.listen(PORT, async () => {
+                    await bot.api.setWebhook(`${WEBHOOK_URL}/${SECRET_PATH}`);
+                    console.log(`[SERVER] Bot starting webhook`);
+                });
+            })
+            .catch((e) => console.error("[DB] Error:", e));
 
         return app;
     }
 
     static async development(bot: Bot<ExtendedContext>): Promise<void> {
-        await Database.connect();
+        bot.catch((err) => {
+            const ctx = err.ctx;
+            console.error(`Error while handling update ${ctx.update.update_id}:`);
+            const e = err.error;
+            if (e instanceof GrammyError) {
+                console.error("Error in request:", e.description);
+            } else if (e instanceof HttpError) {
+                console.error("Could not contact Telegram:", e);
+            } else {
+                console.error("Unknown error:", e);
+            }
+        });
 
         await bot.api.deleteWebhook();
-        console.log("[SERVER] Bot starting polling");
-        await bot.start();
+        await Database.connect()
+            .then(async () => {
+                console.log("[DB] Connected successfully.");
+                console.log("[SERVER] Bot starting polling");
+                await bot.start();
+            })
+            .catch((e) => console.error("[DB] Error:", e));
     }
 }
